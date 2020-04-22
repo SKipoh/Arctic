83c574a2342c786e041a552d6238a334e2df0323
Jack Burne <jack_burne@yahoo.co.uk>
2020-04-22 02:09:23 +0100

    Moving the Nav bar to it's own component and cleaning up App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Code from './pages/code';
 import Electronics from './pages/electronics';
-import Hacking from './pages/hacking';  
+import Hacking from './pages/hacking';
+
+import Nav from './components/navbar';
 
 // Importing any SVGs, Icons or other items as components for use in the
 // program
@@ -26,14 +28,7 @@ import './styles/style.css';
 function App() {
   return (
     <div>
-      <Navbar>
-        <Logo logo={<DoubleChevronIcon/>} text="Burne Tech"/>
-        <NavItem icon={<TestIcon/>} itemText="About"/>
-        <NavItem icon={<CodeIcon/>} itemText="Programming Projects"/>
-        <NavItem icon={<DeathstarIcon/>} itemText="Pen Testing Writeups"/>
-        <NavItem icon={<MicroIcon/>} itemText="Electronics"/>
-        <NavItem icon={<ShuttleIcon/>} itemText="SPAACE"/>
-      </Navbar>
+      <Nav />
       <main>
         <h1>Main Page!</h1>
         <p>grinugeranuipaegfdrniuoperhfdgsanipuohesrfainupherafgniopuaehrfdnuipaherdfnu
@@ -43,47 +38,6 @@ function App() {
       </main>
       
     </div>
-  );
-}
-
-
-function Navbar(props) {
-  // "navbar" is the actual coloured bar that runs down the side of the page,
-  // with navbar-nav being the html list of items INSIDE "navbar".
-  // We can pass in any items we want into. The unorderedlist is the container
-  // for the flexbox
-  return (
-    <nav className="navbar">
-        <ul className="navbar-nav"> { props.children }</ul>
-    </nav>
-  );
-}
-
-
-function NavItem(props) {
-  return (
-    <li className="nav-item">
-      <div class="nav-link">
-        <div>
-          { props.icon }
-        </div>
-        <div class="link-text">
-          {props.itemText}
-        </div>
-      </div>
-    </li>
-  );
-}
-
-
-function Logo(props) {
-  return (
-    <li class="logo">
-      <div className="nav-link">
-        <div class="link-text">{ props.text }</div>
-        <div>{ props.logo }</div>
-      </div>
-    </li>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,34 @@
 import React from 'react';
-import '../App.css';
+import '../styles/style.css';
+
+// Importing any SVGs, Icons or other items as components for use in the
+// program
+import { ReactComponent as TestIcon } from '../icons/address-card.svg';
+import { ReactComponent as CodeIcon } from '../icons/code.svg';
+import { ReactComponent as MicroIcon } from '../icons/microchip.svg';
+import { ReactComponent as DeathstarIcon } from '../icons/deathstar.svg';
+import { ReactComponent as ShuttleIcon } from '../icons/spaceshuttle.svg';
+import { ReactComponent as DoubleChevronIcon } from '../icons/right-double-chevron.svg';
+
+function Navigation() {
+  return (
+    <Navbar>
+        <Logo logo={<DoubleChevronIcon/>} text="Burne Tech"/>
+        <NavItem icon={<TestIcon/>} itemText="About"/>
+        <NavItem icon={<CodeIcon/>} itemText={<div className="linebreak">{"Programming\nProjects"}</div>}/>
+        <NavItem icon={<DeathstarIcon/>} itemText={<div className="linebreak">{"Pen Testing\n Writeups"}</div>}/>
+        <NavItem icon={<MicroIcon/>} itemText="Electronics"/>
+        <NavItem icon={<ShuttleIcon/>} itemText="SPAACE"/>
+      </Navbar>
+  );
+}
+
 
 function Navbar(props) {
+  // "navbar" is the actual coloured bar that runs down the side of the page,
+  // with navbar-nav being the html list of items INSIDE "navbar".
+  // We can pass in any items we want into. The unorderedlist is the container
+  // for the flexbox
   return (
     <nav className="navbar">
         <ul className="navbar-nav"> { props.children }</ul>
@@ -9,14 +36,32 @@ function Navbar(props) {
   );
 }
 
+
 function NavItem(props) {
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
-        {props.icon}
-      </a>
+      <div class="nav-link">
+        <div>
+          { props.icon }
+        </div>
+        <div class="link-text">
+          {props.itemText}
+        </div>
+      </div>
     </li>
-  )
+  );
 }
 
-export default Navbar;
+
+function Logo(props) {
+  return (
+    <li class="logo">
+      <div className="nav-link">
+        <div class="link-text">{ props.text }</div>
+        <div>{ props.logo }</div>
+      </div>
+    </li>
+  );
+}
+
+export default Navigation;
