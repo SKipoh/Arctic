6eeca68418f510e7ff8a37f05667cc58ad9042dd
Jack Burne <jack_burne@yahoo.co.uk>
2020-09-04 21:28:26 +0100

    Adding remaining tabs & importing their logos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,9 @@ import Tile from '../components/tile';
 // Importing Icons
 import { ReactComponent as CodeIcon } from '../icons/CodeDuo.svg';
 import { ReactComponent as MicroIcon } from '../icons/microchipDuo.svg';
+import { ReactComponent as SecretAgentIcon } from '../icons/secretAgentDuo.svg';
+import { ReactComponent as AboutIcon } from '../icons/address-cardDuo.svg';
+import { ReactComponent as LoginIcon } from '../icons/login-Duo.svg';
 
 function Home() {
   return (
@@ -18,16 +21,35 @@ function Home() {
       </div>
 
       <div className="homeText" id="tile">
-        <Tile 
+        <Tile id="tile"
           title="Programming Projects" 
           icon={<CodeIcon/>}
           desc="A variety of programming projects in different languages & areas. Maze Solving, Web Design, Data Science & More"
+          link="/code"
         />
-        <Tile 
+        <Tile id="tile"
           title="Electronics Projects"
           icon={<MicroIcon/>}
           desc="Analogue & Digital electronics projects. Everything from CPU design to guitar effects pedals to 3D printing & Radio Astronomy"
+          link="/electronics"
         />
+        <Tile id="tile"
+          title="Penetration Testing Write-Ups"
+          icon={<SecretAgentIcon/>}
+          desc="Write-Ups from my adventures in Penetration Testing, Usually including sites like HackTheBox & TryHackMe"
+          link="/hacking"
+        />
+        <Tile id="tile"
+          title="About"
+          icon={<AboutIcon/>}
+          desc="About This Website"
+          link="/about"
+        />
+     {/*    <Tile id="tile"
+          title="Login"
+          icon={<LoginIcon/>}
+          desc="Login for moooooore"
+        /> */}
       </div>
     </div>
   );
