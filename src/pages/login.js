02700d27be6c62f4c5d4feec657e8944f099b819
Jack Burne <jack_burne@yahoo.co.uk>
2020-09-05 23:38:12 +0100

    Created a Login Component for User Auth

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as LogoIcon } from '../icons/deathstarDuo.svg';
+import '../styles/style.css';
+
+
+function Login() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
